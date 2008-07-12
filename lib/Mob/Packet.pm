76d454d747a3322a31a425a8e623306e60d5f2d1
Chris Thompson <chris@cthompson.com>
2008-07-11 23:31:55 -0400

    Tidy. Rearrange attributes in the packet.

--- lib/Mob/Packet.pm
+++ lib/Mob/Packet.pm
@@ -11,104 +11,44 @@ use Moose;
 
 use JSON::Any;
 
-has packetID => (
-	isa => 'Int',
-	is=> 'rw',
-);
-
-has URI => (
-	isa => 'Str',
-	is=> 'rw',
-);
-
 has created => (
-	isa => 'Str',
-	is=> 'rw',
-);
-
-has packetType => (
-	isa => 'Int',
-	is=> 'rw',
+    isa => 'Int',
+    is  => 'rw',
 );
 
 has sender => (
-	isa => 'Str',
-	is=> 'rw',
-);
-
-has recipient => (
-	isa => 'Str',
-	is=> 'rw',
+    isa => 'Str',
+    is  => 'rw',
 );
 
 has senderStore => (
-	isa => 'HashRef',
-	is=> 'rw',
+    isa => 'HashRef',
+    is  => 'rw',
 );
 
 has payload => (
-	isa => 'HashRef',
-	is => 'rw',
+    isa => 'HashRef',
+    is  => 'rw',
 );
 
-has cdata => (
-	isa => 'Bool',
-	is=> 'rw',
-	default => 0,
+has event_name => (
+    isa => 'Str',
+    is  => 'ro',
 );
 
-has packetFields => (
-        isa => 'ArrayRef',
-        is => 'ro',
-        default => qw(packetID URI created packetType sender
-                        recipient senderStore payload),
+has venue => (
+    isa     => 'str',
+    is      => 'ro',
+    default => sub { value },
 );
 
-sub create {
-	my $self = shift;
-	my $json = JSON::Any->new;
-	
-	my $packet = ();
-	no strict "subs";
-	
-	foreach my $field ($self->packetFields) {
-			$packet->{$field} =  $self->$field;
-		}
+has routing_constraint => (
+    isa       => 'Maybe[Bool]',
+    is        => 'rw',
+    predicate => 'route_locally',
+    reader    => 'only_route_locally',
+    default   => sub { 0 },
+    clearer   => 'skip_local_routing',
+);
 
-        my $response = $json->objToJson($packet);
-        if ($self->{cdata}) {
-		$response = "<![CDATA[" . $response . "]]>";
-	}
-        return $response;
-}
-
-sub decode {
-	my $self = shift;
-	my $jsonpacket = shift;
-
-	my $json = JSON::Any->new;
-
-   # Always try to strip off CDATA
-	$jsonpacket =~ s/^<![CDATA[(.*?)]]>$/$1/;
-
-	my $packet = $json->jsonToObj($jsonpacket);
-
-   ### if $self is a ref, this is an object call, use the packet to populate
-	### the existing object, overwriting
-
-	if (ref $self) {
-		foreach my $field ($self->packetFields) {
-			$self->$field($packet->{$field});
-  		}
-	} 
-	### if it's not a ref, this is a class method call, use the packet to
-	### create a new object.
-	else 
-	{
-      $self = Mob::Packet->new($packet);
-   }
-
-	### Either way, we're returning a Mob::Packet object
-   return $self;
-}
 1;
