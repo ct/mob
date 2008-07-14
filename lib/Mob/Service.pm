44d0c9afb78e380035447d0d2476daa73551323b
Chris Thompson <chris@cthompson.com>
2008-07-13 21:31:34 -0400

    Add dispatch_request to Mob::Service role

--- lib/Mob/Service.pm
+++ lib/Mob/Service.pm
@@ -9,12 +9,26 @@ our $VERSION = '1.00';
 use strict;
 use Moose::Role;
 
+use Mob::Packet;
+
 has mob_object => (
     isa      => 'Mob',
     is       => 'ro',
     required => 1,
     weaken   => 1,
 );
+
+sub dispatch_request {
+    my ( $self, $args ) = @_;
+
+    my $packet = Mob::Packet->new(
+        sender => self->mob_object->identifier,
+        $args;
+    );
+
+    $self->mob_object->handle_event($packet);
+
+}
 
 no Moose::Role;
 1;
